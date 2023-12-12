5607e36d917501837a9f3f6bbe5c1de7eba864c2
Mateus da Silva <mso3@cin.ufpe.br>
2023-12-12 17:30:48 -0300

    Resolved the implementations of the methods in the child classes, added new properties to the superclass, and created new instances for the child class.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,18 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public int Imei { get; set; }
+        public  int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string model,int imei,int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = model;
+            Imei = imei;
+            Memoria = memoria;
+
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace Desafio
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+
+            
+            Nokia nokia = new Nokia();
+            nokia.InstalarAplicativo("Whatsapp");
+    
+            
+
+        }
+    }
+}
